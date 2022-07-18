dd4be881380b06c1a1cc9aa7d4572dc4a2b5a5e0
tohutohu <tohu.soy@gmail.com>
2022-07-18 18:18:52 +0900

    デプロイの初期設定変更

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -7,11 +7,12 @@ sudo cp -f env.sh /home/isucon/env.sh
 #sudo cp -f etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
 #sudo nginx -t
 
+# アプリケーションのビルド
 #cd /home/isucon/webapp/go
 #go build -o isucondition
 
 # ミドルウェア・Appの再起動
-# sudo systemctl restart mariadb
+#sudo systemctl restart mariadb
 #sudo systemctl reload nginx
 #sudo systemctl restart isucondition.go
 
@@ -21,9 +22,8 @@ set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
 set global long_query_time = 0;
 set global slow_query_log = ON;
 "
-
-# echo $QUERY | sudo mysql -uroot
+echo $QUERY | sudo mysql -uroot
 
 # log permission
-#sudo chmod 777 /var/log/nginx /var/log/nginx/*
-#sudo chmod 777 /var/log/mysql /var/log/mysql/*
+sudo chmod 777 /var/log/nginx /var/log/nginx/*
+sudo chmod 777 /var/log/mysql /var/log/mysql/*
